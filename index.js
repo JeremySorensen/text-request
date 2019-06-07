2c43f35ed24056558cc6394b250d5aeee001f60f
Jeremy Sorensen <Jeremy.Sorensen@analog.com>
2019-06-07 09:39:42 -0700

    adding three.js example

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 // JavaScript source code
-var main = document.getElementById('main');
 
-var myRequest = new Request("https://github.com/JeremySorensen/text-request/blob/master/text.txt?raw=true");
-console.log(myRequest);
-fetch(myRequest).then(function (response) {
-    return response.text().then(function (text) {
-        console.log(text);
-        main.innerHTML = text;
+function getFile() {
+
+    console.log('INIT CALLED!');
+
+    var main = document.getElementById('main');
+
+    var myRequest = new Request(
+        "https://cors.io/?https://raw.githubusercontent.com/JeremySorensen/text-request/master/text.txt",
+        { mode: 'no-cors' });
+    console.log(myRequest);
+    fetch(myRequest).then(function (response) {
+        return response.text().then(function (text) {
+            console.log(text);
+            main.innerHTML = text;
+        });
     });
-});
+}
 
