52b29a2d6e2a1e6ce4f857ba6129a6c6a034f0de
JeremySorensen <JeremySorensen@users.noreply.github.com>
2019-06-06 23:10:25 -0700

    put code in a function

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-// JavaScript source code
-var main = document.getElementById('main');
+function setText() {
+  var main = document.getElementById('main');
 
-var myRequest = new Request(
+  var myRequest = new Request(
     "https://jeremysorensen.github.io/text-request/text.txt"
     { mode: "no-cors" }
-);
-console.log(myRequest);
-fetch(myRequest).then(function (response) {
+  );
+  console.log(myRequest);
+  fetch(myRequest).then(function (response) {
     return response.text().then(function (text) {
-        console.log(text);
-        main.innerHTML = text;
+      console.log(text);
+      main.innerHTML = text;
     });
-});
-
+  });
+}
