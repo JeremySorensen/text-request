21e7c7cad89733276456ecaf9533aa2ed0fdaa44
JeremySorensen <JeremySorensen@users.noreply.github.com>
2019-06-06 23:05:39 -0700

    change url

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // JavaScript source code
 var main = document.getElementById('main');
 
-var myRequest = new Request("https://github.com/JeremySorensen/text-request/blob/master/text.txt?raw=true");
+var myRequest = new Request("https://jeremysorensen.github.io/text-request/text.txt");
 console.log(myRequest);
 fetch(myRequest).then(function (response) {
     return response.text().then(function (text) {
