c0d66cecdff767147e098542faebf0a8a8680d53
JeremySorensen <JeremySorensen@users.noreply.github.com>
2019-06-06 23:06:48 -0700

    added no-cors mode

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 // JavaScript source code
 var main = document.getElementById('main');
 
-var myRequest = new Request("https://jeremysorensen.github.io/text-request/text.txt");
+var myRequest = new Request(
+    "https://jeremysorensen.github.io/text-request/text.txt"
+    { mode: "no-cors" }
+);
 console.log(myRequest);
 fetch(myRequest).then(function (response) {
     return response.text().then(function (text) {
